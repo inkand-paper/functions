6f4d45cd47d1d21431db02786e392cfaf43aec8a
MD. MUSTAK TAHSIN ABIR <153496690+inkand-paper@users.noreply.github.com>
2025-06-05 18:01:37 +0600

    Create basicfunction.kt

---
+++ basicfunction.kt
@@ -0,0 +1,24 @@
+What is Basic function?
+A function is like a named machine: you give it inputs, it gives you output.
+
+
+
+              📦 Syntax:
+                fun add(a: Int, b: Int): Int {
+                    return a + b
+                }
+
+
+
+fun → keyword to create a function
+
+add → function name
+
+a: Int, b: Int → input parameters
+
+: Int → return type
+
+return → sends back the result
+
+🧠 Real Life:
+Like a calculator: you type in numbers and get results.
