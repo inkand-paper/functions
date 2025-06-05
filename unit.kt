46fbe6bc7fb1da17908e71f0151f3459f037ba2b
MD. MUSTAK TAHSIN ABIR <153496690+inkand-paper@users.noreply.github.com>
2025-06-05 18:29:45 +0600

    Create unit.kt

---
+++ unit.kt
@@ -0,0 +1,14 @@
+What is unit return type?
+It means the function returns nothing.
+
+
+📦 Syntax:
+fun sayHi(): Unit {
+    println("Hi")
+}
+
+
+Unit is like void in other languages
+
+Can be skipped; Kotlin assumes it if no return type is written
+
