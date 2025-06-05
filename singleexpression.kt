3528bbea4e6c94a49865d5d04538d62eb1906b85
MD. MUSTAK TAHSIN ABIR <153496690+inkand-paper@users.noreply.github.com>
2025-06-05 18:02:48 +0600

    Create singleexpression.kt

---
+++ singleexpression.kt
@@ -0,0 +1,15 @@
+What is Single Expression Function?
+A shortcut version of a normal function when there is only one line.
+
+
+🧩 Syntax:
+fun square(x: Int) = x * x
+
+
+No {} or return needed
+
+Kotlin guesses the return type
+
+🧠 Real Life:
+Like saying: “If input is 5, just return 25.”
+
