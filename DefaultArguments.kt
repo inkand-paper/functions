e4adfc024571650051a63082f05eeb2148fd4493
MD. MUSTAK TAHSIN ABIR <153496690+inkand-paper@users.noreply.github.com>
2025-06-05 18:04:08 +0600

    Create DefaultArguments.kt

---
+++ DefaultArguments.kt
@@ -0,0 +1,15 @@
+What is Default Arguments?
+You give a default value to a parameter, so callers can skip it.
+
+
+
+📦 Syntax:
+fun greet(name: String = "Friend") {
+    println("Hello, $name")
+}
+
+
+
+🧠 Real Life:
+Like a gift: if no name is written, you write “To: Friend” by default.
+
