1d2dfc7a8c1b309c624bb17089983b465f71e35e
MD. MUSTAK TAHSIN ABIR <153496690+inkand-paper@users.noreply.github.com>
2025-06-05 18:05:06 +0600

    Create NamedArguments.kt

---
+++ NamedArguments.kt
@@ -0,0 +1,15 @@
+What is Named Arguments?
+You call a function using parameter names, not just values.
+
+
+
+📦 Syntax:
+fun greet(name: String, age: Int) {
+    println("Hello $name, age $age")
+}
+
+
+
+greet(name = "Ali", age = 12)
+🧠 Real Life:
+Filling a form: "Name = Ali", "Age = 12"
